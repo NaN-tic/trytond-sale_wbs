acb730ea87865ca0a7205f046e1d241d9f502f32
Guillem Barba <guillem@nan-tic.com>
2015-06-27 14:07:32 +0200

    Add editable basic line fields when line is a child

--- sale.py
+++ sale.py
@@ -177,6 +177,10 @@ class SaleLine(ChapterMixin):
         super(SaleLine, cls).__setup__()
         if hasattr(cls, '_allow_modify_after_draft'):
             cls._allow_modify_after_draft.add('wbs')
+        for field_name in ['product', 'quantity', 'unit', 'unit_price']:
+            field = getattr(cls, field_name)
+            # Remove readonly state defined on sale
+            field.states.update({'readonly': False})
 
     @staticmethod
     def default_left():
